662fc76e3ddea34260d9ca49386cdf59143a3d84
WenHsu1203 <wenhsu1203@g.ucla.edu>
2018-11-19 22:51:38 -0800

    update

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -5,60 +5,12 @@ from keras.models import Sequential
 import cv2
 from keras.preprocessing import image
 import numpy as np
-
-def create_model():
-    model = Sequential()
-    # 224,224,16
-    model.add(Conv2D(filters = 16, kernel_size = 2, padding = 'same', activation = 'relu', input_shape = (224, 224, 3)))
-    # 112,112,16
-    model.add(MaxPooling2D(pool_size = 2))
-    # Dropout
-    model.add(Dropout(0.5))
-    # 112,112,32
-    model.add(Conv2D(filters = 32, kernel_size = 2, padding = 'same', activation = 'relu'))
-    # 56,56,32
-    model.add(MaxPooling2D(pool_size = 2))
-    # Dropout
-    model.add(Dropout(0.5))
-    # 56,56,64
-    model.add(Conv2D(filters = 64, kernel_size = 2, padding = 'same', activation = 'relu'))
-    # 28,28,64
-    model.add(MaxPooling2D(pool_size = 2))
-    # Dropout
-    model.add(Dropout(0.5))
-    # 28,28,128
-    model.add(Conv2D(filters = 128, kernel_size = 2, padding = 'same', activation = 'relu'))
-    # 14,14,128
-    model.add(MaxPooling2D(pool_size = 2))
-    # Dropout
-    model.add(Dropout(0.5))
-    # 14,14,256
-    model.add(Conv2D(filters = 256, kernel_size = 2, padding = 'same', activation = 'relu'))
-    # 7,7,256
-    model.add(MaxPooling2D(pool_size = 2))
-    # Dropout
-    model.add(Dropout(0.5))
-    # flatten
-    model.add(Flatten())
-    model.add(Dense(5000, activation = 'relu'))
-    # Dropout
-    model.add(Dropout(0.5))
-    model.add(Dense(1000, activation = 'relu'))
-    # Dropout
-    model.add(Dropout(0.5))
-    model.add(Dense(500, activation = 'relu'))
-    # Dropout
-    model.add(Dropout(0.5))
-    # Fully connected Layer to the number of signal categories
-    model.add(Dense(4, activation = 'softmax'))
-    return model
-
+from keras.models import load_model
 
 class TLClassifier(object):
     def __init__(self):
         # load classifier
-        self.model = create_model()
-        self.model.load_weights('saved_models/weights.best.self_defined.hdf5')
+        self.model.load_weights('saved_models/weights.test.self_defined.hdf5')
 
     def img_to_tensor(self, img):
         img = cv2.resize(img,(224,224))
